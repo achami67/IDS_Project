e27390cd99c3ddea3786ce94ffeadd80a58516f1
Anas Chami <anaschami@laposte.net>
2024-09-09 15:04:30 +0200

    ids.py script capture les 10 premiers paquets réseau et affiche un résumé de chaque paquet.

--- ids.py
+++ ids.py
@@ -5,4 +5,17 @@ from scapy.all import sniff
 # Cette fonction sera appelé chaque fois qu'un paquet réseau est capturé
 def packet_callback(packet):
 	# Affiche un résumé simple du paquet capture
-	print(packet.sumary())
+	print(packet.summary())
+
+# Fonction principale qui démarre le processus
+def main():
+	# Message d'information indiquant que le système IDS (Intrusion Detection System) est en cours de démarrage
+	print("Démarrage de l'IDS...")
+	
+	#Capture 10 paquets réseau et appelle packet_callback pour chaque paquet capturé
+	sniff(prn=packet_callback, count=30)
+
+#Vérifie si le script est exécuté directement (et non importé dans un autre module)
+if __name__ == '__main__':
+	# Appelle la fonction principale
+	main()
